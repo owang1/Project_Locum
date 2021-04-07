4f202e6fd6cf9f0369fe28d31828fc4ce88b9087
unknown <owang1@nd.edu>
2021-04-07 12:04:32 -0400

    first commit

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'demo');
+
+/* Attempt to connect to MySQL database */
+/* $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME); */
+
+$link = mysqli_connect('localhost', 'jgarci22', 'jgarci22', 'jgarci22')
+    or die('Could not connect: ' . mysql_error());
+echo 'Connected successfully';
+mysqli_select_db($link, 'jgarci22') or die('Could not select database');
+
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
